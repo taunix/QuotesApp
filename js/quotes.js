babb67858c1f56ecd7a5d5a57ae8e7778c6d5d14
Jason Jones <jason.jones@taunix.com>
2021-09-14 20:28:34 -0500

    First commit

---
+++ js/quotes.js
@@ -0,0 +1,11 @@
+quotes = [
+  "You don't need a plan; you just need to be present.|Bob Goff",
+  "It would be a pity if, in a desire (rightly) to treat the Bible as more than a book, we ended up treating it as less than a book by not permitting it the range and use of language, order, and figures of speech that are (or ought to be) familiar to us from our ordinary experience of conversation and reading.|John Lennox",
+  "Nothing is here [Genesis] treated of but the visible form of the world.  He who would learn astronomy, and other recondite arts, let him go elsewhere.|John Calvin",
+  "The Holy Spirit would rather speak childishly than unintelligibly to the humble and unlearned.|John Calvin",
+  "They were blameless not sinless [Zacharias and Elisabeth]...|Matthew Henry",
+  "An institution is a lengthened shadow of a man.|Ralph Waldo Emerson",
+  "Ministry is allowing the Holy Spirit to work in your life and then sharing it with someone else.|Quoted by Nathan Thomas",
+  "There are two reason people are not Christians today:  One, they have never met a Christian; two, they have met a Christian.|Canon J John",
+  "Poor England!  Thou art a devoted deer, Beset with ev'ry ill but that of fear.|William Cowper"
+]
